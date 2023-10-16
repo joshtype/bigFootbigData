15d52ee2e1e97d259222e8d83c474c83771891bc
joshtype <115691670+joshtype@users.noreply.github.com>
2023-10-16 06:55:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// define a function that trains a tensorflow model
+const getPrediction = function() {
+  // adapted from www.tensorflow.org/js/tutorials/setup#browser_setup
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+  model.fit(xs, ys, {epochs: 10}).then(() => {
+    document.getElementById("text").innerHTML = model.predict(tf.tensor2d([5], 
+      [1, 1]));
+  });
+};
+getPrediction();
